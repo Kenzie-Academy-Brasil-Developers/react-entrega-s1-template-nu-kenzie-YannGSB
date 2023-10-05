26ce85de7d091ada7135993ffc689509e6f23281
Yann G. Simonetti Buffet <yanngsb@hotmail.com>
2023-10-05 14:09:24 -0300

    fix: moved to form folder

---
+++ src/components/FinanceForm/Input/index.jsx
@@ -0,0 +1,29 @@
+export const Input = ({
+  label,
+  id,
+  type,
+  placeholder,
+  value,
+  setValue,
+  small,
+  SmallText,
+}) => {
+  return (
+    <div>
+      <label className="body" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        className="input"
+        name={id}
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        required
+      />
+      {small ? <small className="example">{SmallText}</small> : null}
+    </div>
+  );
+};
